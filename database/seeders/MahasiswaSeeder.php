ad719cabdfb12524da49e6edbfc243abd8772a5c
RidwanRizqi <Ridwanrizqi64@gmail.com>
2023-04-12 16:04:24 +0700

    Menambahkan email dan tanggal lahir

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Mahasiswa;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
